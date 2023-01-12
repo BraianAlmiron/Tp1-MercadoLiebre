c0818fa9b208371070495bbece3155bdfb7f9195
BraianAlmiron <braianpatricio1@gmail.com>
2023-01-12 09:59:31 -0300

    formularios terminado

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ app.use(express.static(path.resolve(__dirname,'public')))
 app.listen(port,() => console.log(`Server runner in http://localhost:${port}`));
 
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')))
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
 
